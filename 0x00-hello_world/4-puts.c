b358eccfb57d4bdee48f64b39cb3cdbc65591cf8
OGUH MEDLIN-ALBRIGHT CHIOMA <oguhmedlinalbright@gmail.com>
2023-10-13 07:41:01 -0700

    low_programing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+* main – Entry point
+*
+* Return:  Always 0 (Success)
+* /
+Int main(void)
+{
+   put('/"Programming is like building a multilingual puzzle");
+
+Return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+* main – Entry point
+*
+* Return:  Always 0 (Success)
+* /
+Int main(void)
+{
+   printf("with proper grammar, but the outcome is a piece of art.\n");
+
+Return (0);
+
